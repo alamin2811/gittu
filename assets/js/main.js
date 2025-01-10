29539175ed8f36fbc9303bd1863f7aff9e70976a
alamin2811 <alaminbinmonsur@gmail.com>
2025-01-11 00:01:16 +0600

    modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,34 @@ $(document).ready(function(){
       $('.card-content2, .card-content').toggle();
     });
 });
+
+// progressbar js
+let bar = new ProgressBar.SemiCircle("#container", {
+  strokeWidth: 8,
+  color: "white",
+  trailColor: "rgba(255,255,255, 0.4)",
+  trailWidth: 8,
+  easing: "easeInOut",
+  duration: 1400,
+  svgStyle: null,
+  text: {
+    value: "",
+    alignToBottom: false,
+    className: "progressbar__label"
+  },
+
+  // Set default step function for all animate calls
+  step: (state, bar) => {
+    bar.path.setAttribute("stroke", state.color);
+    var value = Math.round(bar.value() * 100);
+    if (value === 0) {
+      bar.setText("");
+    } else {
+      bar.setText(value);
+    }
+
+    bar.text.style.color = state.color;
+  }
+});
+bar.animate(0.2); // Number from 0.0 to 1.0
 })(jQuery);
